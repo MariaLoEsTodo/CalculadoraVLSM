3166f6b4fb98caa28420faf16fcc8e0337c777c6
Juan Sebastian Barreto Jimenez <63886423+jsebastianbarretoj99@users.noreply.github.com>
2021-09-04 09:17:23 -0500

    ADD filae CalculadoreVLSM_JMSS.py

---
+++ CalculadoraVLSM_JMSS.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+
+root=tk.Tk()
+root.title("Looping of entry box")
+root.geometry("500x600")
+
+def ApplytoLabel():
+    xx=size.get()
+    for i in range(xx):
+        element = box_list[i].get() # Get value from corresponding Entry
+        ArrayLabel=tk.Label(root,text="Array Element: " + element)
+        ArrayLabel.pack()
+
+box_list = []   # Create list of Entrys
+def Boxes():
+    xx=size.get()
+    for i in range(xx):        
+        box=tk.Entry(root)
+        box.pack()
+        box_list.append(box)    # Append current Entry to list
+    ApplytoLabel1=tk.Button(root,text="Calcular",command=ApplytoLabel).place(x=17,y=180)
+    ApplytoLabel1.pack()
+
+Array = tk.Frame(root)
+Array.pack()
+
+text0=tk.Label(Array,text="Dirección IP:",
+               font="Arial 10 bold",fg="blue")
+text0.grid(row=1,column=0,sticky="w")
+
+size0=tk.IntVar()
+
+ArraySize1=tk.Entry(Array,textvariable=size0)
+ArraySize1.grid(row=1,column=1,sticky="w")
+
+text1=tk.Label(Array,text="Prefijo de red:",
+               font="Arial 10 bold",fg="blue")
+text1.grid(row=2,column=0,sticky="w")
+
+size1=tk.IntVar()
+
+ArraySize1=tk.Entry(Array,textvariable=size1)
+ArraySize1.grid(row=2,column=1,sticky="w")
+
+
+text2=tk.Label(Array,text="Número de sub redes:",
+               font="Arial 10 bold",fg="blue")
+text2.grid(row=3,column=0,sticky="w")
+
+size=tk.IntVar()
+
+ArraySize=tk.Entry(Array,textvariable=size)
+ArraySize.grid(row=3,column=1,sticky="w")
+
+    
+SizeofArray=tk.Button(Array,text="Aplicar",command=Boxes)
+SizeofArray.grid(row=3,column=2,sticky="w")
+
+
+root.mainloop()
