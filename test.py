d2ca207ff35ca38e97b63dc60f8e92c887f28b6b
Juan Sebastián Barreto Jiménez <juan_barreto@javeriana.edu.co>
2021-09-06 20:48:55 -0500

    Se elimino archivo de test

--- test.py
+++
@@ -1,9 +0,0 @@
-from tabulate import tabulate
-print("| Red | Hosts | Dirección de red |")
-print ("{:<8} {:<15} {:<10}".format('Red','Hosts','Dirección de red'))
-
-d = [ ["Mark", 12, 95],
-     ["Jay", 11, 88],
-     ["Jack", 14, 90]]
-
-print(tabulate(d, headers=["Red", "Hosts", "Dirección de red"]))
